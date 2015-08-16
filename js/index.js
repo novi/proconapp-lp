73b008a4c0a283697521ea190f50df5b0a760cee
hushin <nnnnot+github@gmail.com>
2015-08-16 20:51:33 +0900

    メールフォームのデザイン

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function(){
+    var $button = $('button[type=submit]');
+    $button.on('click', function(e) {
+        e.preventDefault();
+        
+        $button.prop("disabled", true);
+        
+        var hostUrl = 'https://microsoft-apiappfed672d83d7844978439b52fb1463787.azurewebsites.net/api/Mails/Add';
+        var param1 = $("#emailAddress").val();
+        
+        $.ajax({
+            url: hostUrl,
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                email_address: param1
+            },
+            timeout: 30000,
+            success: function(data) {
+                $('.alert-success').show();
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                $('.alert-danger').show();
+                $button.prop("disabled", false);
+            }
+        });
+    });
+});
